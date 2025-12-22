312d7f0289e7e2782e4988dff69671161a8f237d
Daganep <Daganep@yandex.ru>
2025-12-22 19:29:16 +0300

    Убрал лишние импорты

--- app/src/main/java/ru/coolnotes/navigation/Navigation.kt
+++ app/src/main/java/ru/coolnotes/navigation/Navigation.kt
@@ -1,11 +1,7 @@
 package ru.coolnotes.navigation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
@@ -14,22 +10,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
 import com.openkin.presentation.navigation.AppRouting
 import com.openkin.presentation.navigation.Screen
-import com.openkin.presentation.ui.notesboard.NotesBoard
 import com.openkin.presentation.ui.Settings
 import com.openkin.presentation.ui.Splash
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import com.openkin.presentation.ui.addnote.AddNoteScreen
 import com.openkin.presentation.ui.archive.ArchiveScreen
+import com.openkin.presentation.ui.notesboard.NotesBoard
 import ru.coolnotes.navigation.navigationbar.NavigationBar
 
 @Composable
--- presentation/src/main/java/com/openkin/presentation/ui/notesboard/widgets/HorizontalNote.kt
+++ presentation/src/main/java/com/openkin/presentation/ui/notesboard/widgets/HorizontalNote.kt
@@ -1,30 +1,28 @@
 package com.openkin.presentation.ui.notesboard.widgets
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.openkin.presentation.ui.model.NoteUi
-import androidx.compose.material3.Text
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.openkin.presentation.R
+import com.openkin.presentation.ui.model.NoteUi
 
 @Composable
 fun HorizontalNote(
