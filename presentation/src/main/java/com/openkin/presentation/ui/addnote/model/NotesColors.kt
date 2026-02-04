d3ff126ccc152f6f3c5b21629d27efe3fa4b91df
Daganep <Daganep@yandex.ru>
2026-02-04 14:23:52 +0300

    Поправил розовый цвет

--- presentation/src/main/java/com/openkin/presentation/ui/addnote/model/NotesColors.kt
+++ presentation/src/main/java/com/openkin/presentation/ui/addnote/model/NotesColors.kt
@@ -7,7 +7,7 @@ enum class NotesColors(val startColor: Color, val endColor: Color) {
     Red(startColor = Color(0xFFFD8484), endColor = Color(0xFFFF3232)),
     Green(startColor = Color(0xFFDAFFDA), endColor = Color(0xFF91FF91)),
     Blue(startColor = Color(0xFFB5F0FF), endColor = Color(0xFF63EDFF)),
-    Pink(startColor = Color(0xFFFAB9FA), endColor = Color(0xFFFF7DFF)),
+    Pink(startColor = Color(0xFFFFA5F7), endColor = Color(0xFFFF59F7)),
     Gray(startColor = Color(0xFFF6F6F6), endColor = Color(0xFFCCCCCC)),
     Purple(startColor = Color(0xFFD99DF1), endColor = Color(0xFFD65AF8)),
 }
