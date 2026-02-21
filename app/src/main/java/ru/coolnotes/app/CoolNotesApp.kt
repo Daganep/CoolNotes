2eb8cf949072f359cabbc1cf8232614580a84ede
Daganep <Daganep@yandex.ru>
2026-02-21 10:33:08 +0300

    Добвил очистку состояния в DataStore при запуске приложения

--- app/src/main/java/ru/coolnotes/app/CoolNotesApp.kt
+++ app/src/main/java/ru/coolnotes/app/CoolNotesApp.kt
@@ -1,17 +1,27 @@
 package ru.coolnotes.app
 
 import android.app.Application
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import ru.coolnotes.di.appModule
 import ru.coolnotes.di.viewModelModule
+import ru.coolnotes.extensions.StateDataStore
 
 class CoolNotesApp : Application() {
 
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     override fun onCreate() {
         super.onCreate()
+
+        applicationScope.launch { this@CoolNotesApp.StateDataStore.edit { it.clear() } }
 
         startKoin {
             androidLogger(Level.DEBUG)
